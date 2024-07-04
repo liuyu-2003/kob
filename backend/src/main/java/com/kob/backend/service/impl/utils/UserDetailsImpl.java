4e8f5612ccc72cd7e25203f9d0e862632f63964a
liuyu-2003 <93027320+liuyu-2003@users.noreply.github.com>
2024-07-04 15:47:29 +0800

    配置Mysql与注册登录模块，SpringBoot2同步

--- backend/src/main/java/com/kob/backend/service/impl/utils/UserDetailsImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/utils/UserDetailsImpl.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -19,7 +18,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return null;
     }
 
     @Override
@@ -34,21 +33,21 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
